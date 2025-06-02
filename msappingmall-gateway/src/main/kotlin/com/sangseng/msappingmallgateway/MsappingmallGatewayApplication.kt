1978a1454ed4e5083ee679f8b9f090737ae0e1d8
nn_che <44915367+miche715@users.noreply.github.com>
2025-06-02 23:29:10 +0900

    [FEATURE]<gateway>초기 구현.

---
+++ msappingmall-gateway/src/main/kotlin/com/sangseng/msappingmallgateway/MsappingmallGatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.sangseng.msappingmallgateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsappingmallGatewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<MsappingmallGatewayApplication>(*args)
+}
---
+++ msappingmall-gateway/src/test/kotlin/com/sangseng/msappingmallgateway/MsappingmallGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sangseng.msappingmallgateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsappingmallGatewayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
