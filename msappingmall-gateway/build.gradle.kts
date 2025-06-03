16d281475642e956f8aee53b52eb750cc3fd155a
nn_che <44915367+miche715@users.noreply.github.com>
2025-06-03 23:46:20 +0900

    [FEATURE]<gateway>코드 정리.

--- msappingmall-gateway/build.gradle.kts
+++ msappingmall-gateway/build.gradle.kts
@@ -22,10 +22,12 @@ extra["springCloudVersion"] = "2025.0.0"
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
     implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webmvc")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 dependencyManagement {
